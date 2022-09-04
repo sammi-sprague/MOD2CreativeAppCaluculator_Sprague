69125e3d9abe7f89175311dd9df65f4ba178542f
Sammi Sprague <samantha.sprague23@d155.org>
2022-09-03 20:40:24 -0500

    Initial Commit

---
+++ MOD2CreativeAppCaluculator_Sprague/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MOD2CreativeAppCaluculator_Sprague
+//
+//  Created by SAMANTHA SPRAGUE on 9/3/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
